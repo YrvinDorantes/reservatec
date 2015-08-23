d7582fcd9ab1f89731784376061bed1f84c20d72
Rafael García <rafagarciafdz@gmail.com>
2015-08-23 03:29:47 -0500

    Polymorphosis implementation and configuration in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :reservations
+  has_many :reservations, as: :reservable
+  has_many :place_reservations, through: :reservations, source: :place
+  has_many :stuff_reservations, through: :reservations, source: :stuff
 end
