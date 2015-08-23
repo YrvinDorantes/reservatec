1e98ad1656dfa63f2999f6e80b47416944adff57
Rafael García <rafagarciafdz@gmail.com>
2015-08-23 03:32:12 -0500

    Polymorphosis implementation and configuration (migration)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150823080757) do
+ActiveRecord::Schema.define(version: 20150823082239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,10 +69,13 @@ ActiveRecord::Schema.define(version: 20150823080757) do
     t.integer  "status"
     t.string   "userapproved"
     t.integer  "user_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.integer  "reservable_id"
+    t.string   "reservable_type"
   end
 
+  add_index "reservations", ["reservable_type", "reservable_id"], name: "index_reservations_on_reservable_type_and_reservable_id", using: :btree
   add_index "reservations", ["user_id"], name: "index_reservations_on_user_id", using: :btree
 
   create_table "roles", force: :cascade do |t|
