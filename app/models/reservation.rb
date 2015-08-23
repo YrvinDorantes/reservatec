e6da0f68b88543d02627947bcf903a61879343a1
Rafael García <rafagarciafdz@gmail.com>
2015-08-23 03:52:28 -0500

    Insertion of enum estatus into reservation.rb

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,6 @@ class Reservation < ActiveRecord::Base
   belongs_to :user
 
   belongs_to :reservable, polymorphic: true
+
+  enum status: [:"Reservando...", :"Cancelada", :"Pendiente", :"Aceptada", :"Finalizada"]
 end
